43053c7feecc7edf51af8560b8f71c0fa0f9f39e
Ansh <patelansh883@gmail.com>
2023-06-06 13:44:30 +0530

    Disable GSAP

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 //   smooth: true,
 // });
 
-gsap.from([".nav", ".heroName", ".content"], {
-  x: "250vh",
-  duration: 1,
-  //   delay: 0.5,
-  stagger: 0.25,
-  ease: "power2.inOut",
-});
+// gsap.from([".nav", ".heroName", ".content"], {
+//   x: "250vh",
+//   duration: 1,
+//   //   delay: 0.5,
+//   stagger: 0.25,
+//   ease: "power2.inOut",
+// });
 
 const elementToRemove = document.getElementsByClassName("content");
 const elementToAppend = document.getElementsByClassName("projects");
